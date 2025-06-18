39268effc4fa729c379b7a933c703e0cc17d91be
cbtruong <batruong12122002@gmail.com>
2025-06-18 15:35:53 +0700

    Create best seller, new letter bos, our policy component functionality UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PlaceOrder from './pages/PlaceOrder';
 import {Routes, Route} from 'react-router-dom'
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/place-order' element={<PlaceOrder/>}/>
         <Route path='orders' element={<Orders />}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
