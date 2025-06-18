44bbbefc7728df3d6ca7b9f3f5fc757d35a20d1f
cbtruong <batruong12122002@gmail.com>
2025-06-18 10:22:32 +0700

    build project and router

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Product from './pages/Product';
+import Login from './pages/Login';
+import Collection from './pages/Home';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import {Routes, Route} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='orders' element={<Orders />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
