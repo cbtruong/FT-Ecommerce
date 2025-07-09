f26d3a7c62a143055bbb9c4e46160ea7f811cabf
cbtruong <batruong12122002@gmail.com>
2025-07-10 02:17:52 +0700

    create sidebar component ui

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import {Routes, Route} from 'react-router-dom';
 import Add from './pages/Add';
+import Sidebar from './components/Sidebar';
 const App = () => {
   return (
-    <>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
       <Navbar/>
+      <hr/>
+      <div className='flex w-full'>
+        <Sidebar/>
+      </div>
       <Routes>
         <Route path='' element={<Add/>}/>
       </Routes>
-    </>
+      </>
+    </div>
   )
 }
 
